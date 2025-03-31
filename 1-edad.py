5ad6e4b82fd5cd502641c8966f66c01675a6f64d
A-monardes <monardesagustin@gmail.com>
2025-03-31 21:38:46 +0200

    TP3 de Programacion I

---
+++ 1-edad.py
@@ -0,0 +1,6 @@
+# 1) Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años,
+# deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”.
+
+num = int(input("Introduzca su edad: "))
+if num >= 18:
+    print("Es mayor de edad")
